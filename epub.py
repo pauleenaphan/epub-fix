7645c08253356c22f2882612c6c6c856a2d1140e
pauleena phan <pauleena2002@gmail.com>
2023-09-05 19:37:20 -0700

    tried to remove imgs

--- epub.py
+++ epub.py
@@ -8,27 +8,29 @@ import os
 def flip_image(image):
     return ImageOps.flip(image)
 
-# Function to remove <img> elements from a specific class in an HTML file
-def remove_images_from_class(html_content, class_name):
+# Function to remove src attributes from images within a specific class in an HTML file
+def remove_src_attributes_from_class(html_content, class_name):
     soup = BeautifulSoup(html_content, 'html.parser')
     elements_to_remove = soup.find_all(class_=class_name)
     
     for element in elements_to_remove:
-        # Find all <img> elements within the class and remove them
+        # Find all <img> elements within the class and remove their src attribute
         for img_element in element.find_all('img'):
-            img_element.decompose()  # Remove the entire <img> element
+            img_element['src'] = ''
     
     return str(soup)
 
+# Function to remove specified text from HTML
+def remove_text_from_html(html_content, text_to_remove):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    for element in soup.find_all(text=True):
+        if text_to_remove in element:
+            element.replace_with(element.replace(text_to_remove, ''))
 
-# Function to remove text content from an HTML file
-def remove_text_from_html(html_content):
-    # Decode bytes to string, replace text, and encode back to bytes
-    text = html_content.decode('utf-8')
-    text_without_pattern = text.replace("I l@ve RuBoard", "")
-    return text_without_pattern.encode('utf-8')
+    return str(soup)
 
-# Function to create a new EPUB file with removed images from specified classes
+
+# Function to create a new EPUB file with removed src attributes from images in the "calibre_6" class
 def create_modified_epub(input_epub, output_epub):
     # Create a temporary directory to extract the EPUB content
     temp_dir = "temp_epub"
@@ -51,9 +53,8 @@ def create_modified_epub(input_epub, output_epub):
                     flipped_image.save(flipped_image_bytes, format=image.format)
                     content = flipped_image_bytes.getvalue()
                 elif file_path.endswith('.html'):
-                    # Remove images from the "calibre_6" and "calibre_1" classes
-                    content = remove_images_from_class(content, 'calibre_6')  # Remove from calibre_6 class
-                    content = remove_images_from_class(content, 'calibre_1')  # Remove from calibre_1 class
+                    # Remove specified text from HTML content
+                    content = remove_text_from_html(content, "I l@ve RuBoard")  # Specify the text to remove
 
                 arcname = os.path.relpath(file_path, temp_dir)
                 new_epub_file.writestr(arcname, content)
